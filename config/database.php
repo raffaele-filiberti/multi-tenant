ff9d4e36c4f62c5e901a9e50ba731c44b2f0905d
restful-crm <raffaele.filiberti@heartweb.it>
2017-03-11 19:08:37 +0100

    all

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql-raffaelefiliberti.alwaysdata.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'raffaelefiliberti_multi_tenancy'),
+            'username' => env('DB_USERNAME', '127611'),
+            'password' => env('DB_PASSWORD', 'hw!work862'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
