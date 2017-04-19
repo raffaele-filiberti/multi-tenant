1cd2709a292d4d69f306eaf65baf026af8e396d5
restful-crm <raffaele.filiberti@heartweb.it>
2017-04-19 08:20:07 +0200

    test

--- app/Api/V1/Requests/UserRequest.php
+++ app/Api/V1/Requests/UserRequest.php
@@ -39,8 +39,8 @@ class UserRequest extends FormRequest
                     'name' => 'required|max:255',
                     'email' => 'required|unique:users|email',
                     'password' => 'required|min:6',
-                    'customer_id' => 'required',
-                    'role_id' => 'required'
+//                    'customer_id' => 'required',
+//                    'role_id' => 'required'
 
                 ];
             }
