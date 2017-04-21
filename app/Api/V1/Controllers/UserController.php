63aa68659b733802bf5af7fd7e3ad77bfe15ef4a
restful-crm <raffaele.filiberti@heartweb.it>
2017-04-21 15:34:04 +0200

    add customer relationship

--- app/Api/V1/Controllers/UserController.php
+++ app/Api/V1/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         return Response()->json([
-            'users' => User::with('roles', 'customersg')->get()
+            'users' => User::with('roles', 'customers')->get()
         ]);
     }
 
