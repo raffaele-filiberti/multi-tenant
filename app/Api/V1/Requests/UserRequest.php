681c72d38dce5a8348e23f89d02d0dc676c93deb
restful-crm <raffaele.filiberti@heartweb.it>
2017-04-22 01:16:03 +0200

    allow email updating

--- app/Api/V1/Requests/UserRequest.php
+++ app/Api/V1/Requests/UserRequest.php
@@ -51,7 +51,7 @@ class UserRequest extends FormRequest
                     'customer_id' => 'integer|nullable',
                     'role_id' => 'integer|nullable',
                     'name' => 'max:255',
-                    'email' => 'unique:users,email,'.$this->route('users'),
+                    'email' => 'unique:users|email|'.$this->route('users'),
                     'password' => 'min:6',
                     'avatar_path' => 'image|mimes:jpg,png|max:5000'
                 ];
