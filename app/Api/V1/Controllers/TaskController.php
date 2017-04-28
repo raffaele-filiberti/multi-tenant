bb17d549e946b69fee84c3aff4fba518d7af5fda
restful-crm <raffaele.filiberti@heartweb.it>
2017-04-28 15:39:55 +0200

    detail json

--- app/Api/V1/Controllers/TaskController.php
+++ app/Api/V1/Controllers/TaskController.php
@@ -27,7 +27,7 @@ class TaskController extends Controller
             return Response()->json([
                 'tasks' => Project::find($project_id)->tasks()
                     ->where('archivied', '=', false)
-                    ->with('steps')
+                    ->with('steps', 'steps.details')
                     ->get()
             ]);
         }
