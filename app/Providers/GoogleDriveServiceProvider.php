7a236a5fc12031ffb1b26185255256379742497a
restful-crm <raffaele.filiberti@heartweb.it>
2017-05-01 15:13:04 +0200

    test google

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -16,16 +16,16 @@ class GoogleDriveServiceProvider extends ServiceProvider
             $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
             $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
             $client->refreshToken(env('GOOGLE_DRIVE_REFRESH_TOKEN'));
-//            $client->setAccessType('offline');
-//            $client->setIncludeGrantedScopes(true);
-//            $client->addScope(\Google_Service_Drive::DRIVE);
-//            $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-//            $client->setRedirectUri($redirect_uri);
-//
-//            if (isset($_GET['code'])) {
-//                $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-//                $client->setAccessToken($token);
-//            }
+            $client->setAccessType('offline');
+            $client->setIncludeGrantedScopes(true);
+            $client->addScope(\Google_Service_Drive::DRIVE);
+            $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+            $client->setRedirectUri($redirect_uri);
+
+            if (isset($_GET['code'])) {
+                $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+                $client->setAccessToken($token);
+            }
 
             $service = new \Google_Service_Drive($client);
 
