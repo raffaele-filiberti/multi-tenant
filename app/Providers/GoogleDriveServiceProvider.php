be36af278598b49560cdbf1c7da21903df8370e1
restful-crm <raffaele.filiberti@heartweb.it>
2017-05-01 13:39:59 +0200

    test google

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -13,11 +13,11 @@ class GoogleDriveServiceProvider extends ServiceProvider
     {
         \Storage::extend('google', function($app, $config) {
             $client = new \Google_Client();
-            $client->setClientId($config['clientId']);
-            $client->setClientSecret($config['clientSecret']);
-            $client->refreshToken($config['refreshToken']);
+            $client->setClientId(env(GOOGLE_DRIVE_CLIENT_ID));
+            $client->setClientSecret(env(GOOGLE_DRIVE_CLIENT_SECRET));
+            $client->refreshToken(env(GOOGLE_DRIVE_REFRESH_TOKEN));
             $service = new \Google_Service_Drive($client);
-            $adapter = new \Hypweb\Flysystem\GoogleDrive\GoogleDriveAdapter($service, $config['folderId']);
+            $adapter = new \Hypweb\Flysystem\GoogleDrive\GoogleDriveAdapter($service, GOOGLE_DRIVE_FOLDER_ID);
             return new \League\Flysystem\Filesystem($adapter);
         });
     }
