99d73c6c4076bcbd0c1319722adcfbf44d28d44f
restful-crm <raffaele.filiberti@heartweb.it>
2017-05-02 16:17:58 +0200

    set upload

--- app/Api/V1/Controllers/FileController.php
+++ app/Api/V1/Controllers/FileController.php
@@ -14,14 +14,13 @@ class FileController extends Controller
 {
     public function storeStepFiles(Request $request, $customer_id, $project_id, $task_id)
     {
-        $file = $request->file('file');
 
-        Storage::disk('google')->put('tests', $request->file('file'));
+        Storage::disk('google')->put('tests', file_get_contents($request->file('file')->getRealPath()));
 
         return response()->json([
             'description' => $request->input('description'),
             'step_task_id' => $request->input('step_task_id'),
-            'file' => $file->getClientOriginalName()
+            'file' => 'ok'
         ]);
     }
 }
