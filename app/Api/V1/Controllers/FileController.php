a7bf6dfb6d5ace540a2d5291c9d801447c1558ce
restful-crm <raffaele.filiberti@heartweb.it>
2017-05-03 20:40:46 +0200

    set upload

--- app/Api/V1/Controllers/FileController.php
+++ app/Api/V1/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Api\V1\Controllers;
 
 use App\Api\V1\Requests\FileRequest;
+use App\Detail_Step_Task;
 use Dingo\Api\Http\Request;
 
 use App\File;
@@ -15,13 +16,22 @@ class FileController extends Controller
     public function storeStepFiles(Request $request, $customer_id, $project_id, $task_id)
     {
         $extension = $request->file('file')->getClientOriginalExtension();
+        $google_path = 'tests/';
         $contents = ( $extension == 'jpg' || $extension == 'png' )? file_get_contents($request->file('file')->getRealPath()) : utf8_encode(file_get_contents($request->file('file')->getRealPath()));
-        Storage::disk('google')->put($request->file('file')->getClientOriginalName(), $contents);
 
+        Storage::disk('google')->put($google_path . trim($request->file('file')->getClientOriginalName()), $contents);
+
+        Detail_Step_Task::find($request->input('step_task_id'))->files()->create([
+            'filename' => trim($request->file('file')->getClientOriginalName()),
+            'description' => $request->input('description'),
+            'path' => $google_path,
+            'mime' => $request->file('file')->getClientMimeType(),
+            'size' => $request->file('file')->getClientSize()
+        ]);
 
         return response()->json([
             'description' => $request->input('description'),
-            'step_task_id' => $request->input('step_task_id'),
+            'detail_step_task_id' => $request->input('step_task_id'),
             'file' => $request->file('file')->getClientOriginalName(),
             'file_content' => utf8_encode(file_get_contents($request->file('file')->getRealPath()))
         ]);
